3be82890307632bfe0e94bb130ed0d70c34536b8
Saptaparno <ss3598@srmist.edu.in>
2024-07-01 15:29:01 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+![image](https://github.com/hmittal28/behance_clone/assets/107806450/63b546b5-7b48-47d8-b585-ef69f55d7b1e)
+# Getting Started with Behance Clone
+
+
+
+This is a Clone project of Behance Website (https://www.behance.net/)
+This project is made to implement the frontend features and components of behance website.
+This project is made mainly with technologies:
+1. React-app
+2. Tailwind.css
+3. React-icons.
+
+This project posses feature like iconic bahance header bar following with searchbar and behance-tags. Then on the next part a filter component is rendored which showcase many type of filters and reccomendation which can be implemented or used to filter the rendored data.
+
+Some Key features of the Website:
+1. Search bar at the top of the page 
+2. Category Dropdown or Menu to sort different categories Projects 
+3. Like an Image - Clicking on the like button simulate likes of the image 
+4. Responsive grid and Grid image zoom in-out on hover 
+
+Then the project contain a responsive and attractive grid consist of data contents such as img, name, writter, total likes and views. As a last part , a footer is created with all the essential option needed. It also posses a custom user made fevicon icon.
+
+# To run and host this project in local server follow the following steps:
+1. install react, node and create a folder and open in editor.
+2. After opening folder in editor install all dependencies and node modules.
+3. Install tailwind,css and react-icon. Confirm then in .json file.
+4. After all this setup download or copy these code files to the project.
+5. run npm start and projec toj local host.
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+Live link of the project:
+- (https://66536d89c5217297dcce1d68--quiet-buttercream-66f100.netlify.app/)
+This project is hosted on a app Netlify -A free web hosting website. 
---
+++ src/Data.js
@@ -0,0 +1,155 @@
+
+import { AiFillLike } from 'react-icons/ai';
+import { AiTwotoneEye } from 'react-icons/ai';
+
+
+
+export const behanceItem = [
+    {
+        
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://st.depositphotos.com/1008939/1679/i/450/depositphotos_16793955-stock-photo-black-beauty.jpgx',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'Sa'
+    },
+    {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://t3.ftcdn.net/jpg/03/54/17/86/360_F_354178616_uSdqA6i1A1vkkskFPKOoxQOED0ZMIcn3.jpg',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'Saa'
+    }, {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/4468e2158061195.Y3JvcCw0MzE0LDMzNzUsMjg4LDA.jpg',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'Saa'
+    }, {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://st5.depositphotos.com/79687876/67455/i/380/depositphotos_674557830-stock-photo-home-hobby-drawing-home-art.jpg',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'SAA'
+    }, {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/55a92a152718637.Y3JvcCwxMTkyLDkzMywxMzIsMA.jpg',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'Re'
+    },
+    {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://st.depositphotos.com/1008939/1679/i/450/depositphotos_16793955-stock-photo-black-beauty.jpgx',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'sho'
+    }, {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://t3.ftcdn.net/jpg/03/54/17/86/360_F_354178616_uSdqA6i1A1vkkskFPKOoxQOED0ZMIcn3.jpg',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'Reshoka'
+    },
+    {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/4468e2158061195.Y3JvcCw0MzE0LDMzNzUsMjg4LDA.jpg',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'ka'
+    },
+    {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/aadca8140674779.Y3JvcCwxMzgwLDEwODAsMjQyLDA.jpg',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'ka'
+    },
+    {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/aadca8140674779.Y3JvcCwxMzgwLDEwODAsMjQyLDA.jpg',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'ok'
+    },
+    {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/24806c126581961.Y3JvcCwxMzgwLDEwODAsMjU2LDA.jpg',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'a'
+    },
+    {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/a08655129351935.Y3JvcCw5OTQsNzc4LDIyLDA.png',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'qq'
+    },
+    {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://st.depositphotos.com/1008939/1679/i/450/depositphotos_16793955-stock-photo-black-beauty.jpgx',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'root'
+    },
+    {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/15b0fc157771815.Y3JvcCwxMTQzLDg5NCwxMjcsMjU.png',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'vvv'
+    },
+    {
+        // fePatch: 'https://a5.behance.net/eb6198339b5bd9210ee85c5041d2da69b52ae5a5/img/galleries/icons/ribbons/1x/graphic-design.png?cb=264615658',
+        featureImg: 'https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/55a92a152718637.Y3JvcCwxMTkyLDkzMywxMzIsMA.jpg',
+        featureTxt: 'Freeze-dried Cat Treats—Wanpy',
+        feLike: '51',
+        felikeIcons: <AiFillLike />,
+        feWatch: '580',
+        fewatchIcon: <AiTwotoneEye />,
+        feUser: 'ppp'
+    },
+]
